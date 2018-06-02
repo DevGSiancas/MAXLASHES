5a83cedf30e7359cc49ac7d68127f4bc79f728bd
Gino Siancas <ginito_lee@hotmail.com>
2018-06-02 16:00:08 -0500

    cambio de htaccess y logo tipo nuevo

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,9 +1,21 @@
 <?php
 
+############################################
+# For development                          #
+############################################
+// define("HOST" , "localhost");
+// define("PASS" , "Max#123");
+// define("USER" , "maxlashes");
+// define("DB" , "max_demo");
+
+############################################
+# For  production                          #
+############################################
 define("HOST" , "localhost");
-define("PASS" , "Max#123");
-define("USER" , "maxlashes");
-define("DB" , "max_demo");
+define("PASS" , "M@x1@2h32#123%!");
+define("USER" , "maxlashes_con");
+define("DB" , "maxlashes_2018_6");
+
 
 $connect = mysqli_connect(HOST , USER , PASS , DB) or die("NO PUDIMOS CONECTAR AL SERVIDOR.");
 
